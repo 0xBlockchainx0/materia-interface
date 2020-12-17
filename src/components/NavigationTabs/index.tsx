fbd2d9e323b304ce6630665594611c98055307d4
Mirco Ramundo <mramundo@outlook.it>
2020-12-17 09:23:14 +0100

    Fixed label

--- src/components/NavigationTabs/index.tsx
+++ src/components/NavigationTabs/index.tsx
@@ -59,7 +59,7 @@ export function FindPoolTabs() {
         <HistoryLink to="/pool">
           <StyledArrowLeft />
         </HistoryLink>
-        <ActiveText>Import Pool AAA</ActiveText>
+        <ActiveText>Import Pool</ActiveText>
         <QuestionHelper text={"Use this tool to find pairs that don't automatically appear in the interface."} />
       </RowBetween>
     </Tabs>
