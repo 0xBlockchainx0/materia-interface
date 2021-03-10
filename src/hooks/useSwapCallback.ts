21c364d129ca1eb1444030c286a55e788dac3f51
Mirco Ramundo <mramundo@outlook.it>
2021-03-10 17:11:09 +0100

    Removed swapCalls logs

--- src/hooks/useSwapCallback.ts
+++ src/hooks/useSwapCallback.ts
@@ -121,8 +121,6 @@ export function useSwapCallback(
   const swapCalls = useSwapCallArguments(trade, allowedSlippage, recipientAddressOrName, tokenIn, tokenOut, etherIn, etherOut)
   const addTransaction = useTransactionAdder()
 
-  console.log('swapCalls: ', swapCalls)
-
   const { address: recipientAddress } = useENS(recipientAddressOrName)
   const recipient = recipientAddressOrName === null ? account : recipientAddress
 
