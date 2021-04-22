fa9f716d32d31d4c023bfb507e5f4ce2a697f5d2
Mirco Ramundo <mramundo@outlook.it>
2021-04-22 17:37:14 +0200

    Updated STAKING_GENESIS for LM Season 2

--- src/state/stake/hooks.ts
+++ src/state/stake/hooks.ts
@@ -6,7 +6,7 @@ import { useActiveWeb3React } from '../../hooks'
 import { NEVER_RELOAD, useMultipleContractSingleData } from '../multicall/hooks'
 import { tryParseAmount } from '../swap/hooks'
 
-export const STAKING_GENESIS = 1619088600
+export const STAKING_GENESIS = 1619121600
 export const REWARDS_DURATION_DAYS = 60
 
 export enum StakingSeason {
